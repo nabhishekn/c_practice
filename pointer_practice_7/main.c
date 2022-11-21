890de9666175ca9c15356fdeb678d0fe1303a066
nabhishekn <abhisheknn1994@gmail.com>
2022-11-21 18:30:38 +0530

    Addition subtraction using pointers

---
+++ pointer_practice_7/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int sum = (*a + *b), diff;
+    if(*a > *b){
+        diff = (*a - *b);
+    }
+    else{
+        diff = (*b - *a);
+    }
+    *a = sum;
+    *b = diff;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
